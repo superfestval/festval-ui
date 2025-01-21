8a8eb21014058e94c012dee312de074dfc287d1a
daniellvaz <daniellmvaz@gmail.com>
2025-01-21 14:57:16 -0300

    fix(form/select): remove margin bottom

--- src/components/ui/form/select/trigger.tsx
+++ src/components/ui/form/select/trigger.tsx
@@ -6,7 +6,7 @@ export function Trigger({ children, ...rest }: SelectTriggerProps) {
   return (
     <SelectPrimitive.Trigger
       {...rest}
-      className="mb-4 flex w-full items-start justify-between rounded border-2 border-zinc-200 bg-zinc-50 p-2 outline-yellow-700"
+      className="flex w-full items-start justify-between rounded border-2 border-zinc-200 bg-zinc-50 p-2 outline-yellow-700"
     >
       {children}
     </SelectPrimitive.Trigger>
