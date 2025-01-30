d93ce06d6e1bdc767683fe794409f70ac681dc21
daniellvaz <daniellmvaz@gmail.com>
2025-01-30 15:59:16 -0300

    chore: build

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -6,6 +6,6 @@ addons.setConfig({
     ...themes.dark,
     brandTitle: "Super Festval | UI Kit",
     brandImage: "/logo.svg",
-    brandUrl: "https://superfestval.github.io/festval-ui",
+    brandUrl: "https://superfestval.github.io/festval-ui/public",
   },
 });
