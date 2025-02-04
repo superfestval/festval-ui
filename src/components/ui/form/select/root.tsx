3deb13bfd4b66f4aae77684dd4903f94abeb9b7e
daniellvaz <daniellmvaz@gmail.com>
2025-02-04 15:48:09 -0300

    fix: remove unused props

--- src/components/ui/form/select/root.tsx
+++ src/components/ui/form/select/root.tsx
@@ -4,9 +4,14 @@ export type SelectRootProps = {
   error?: string;
 } & SelectPrimitive.SelectProps;
 
-export function Root({ children, error, ...rest }: SelectRootProps) {
+export function Root({
+  children,
+  error,
+  defaultValue,
+  ...rest
+}: SelectRootProps) {
   return (
-    <SelectPrimitive.Root {...rest}>
+    <SelectPrimitive.Root {...rest} defaultValue={defaultValue}>
       {children}
       {error && <small className="mt-1 text-xs text-rose-600">{error}</small>}
     </SelectPrimitive.Root>
