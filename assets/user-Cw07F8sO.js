5bbc3ff8e479905d18df15ec6ebfb162379f7cd6
daniellvaz <daniellmvaz@gmail.com>
2025-02-06 17:16:53 +0000

    Deploying to gh-pages from @ superfestval/festval-ui@0d3cf230996c1c4e8d81eda44501a93a32da0449 🚀

---
+++ assets/user-Cw07F8sO.js
@@ -0,0 +1,11 @@
+import{c as e}from"./createLucideIcon-BT4tydPZ.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("Bell",[["path",{d:"M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9",key:"1qo2s2"}],["path",{d:"M10.3 21a1.94 1.94 0 0 0 3.4 0",key:"qgo35s"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=e("User",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]);export{c as B,s as U};
