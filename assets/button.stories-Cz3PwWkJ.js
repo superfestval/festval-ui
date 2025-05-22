4c3e1e264edd3499b0a999f3f8c6efa3280edcb5
daniellvaz <daniellmvaz@gmail.com>
2025-05-22 17:21:35 +0000

    Deploying to gh-pages from @ superfestval/festval-ui@b9376674112b27e384dafc086006e3961aeedc95 🚀

---
+++ assets/button.stories-Cz3PwWkJ.js
@@ -0,0 +1,53 @@
+import{j as q}from"./jsx-runtime-z8MfsBtr.js";import{B as i}from"./index-WPAokm3T.js";import{c as E}from"./createLucideIcon-BT4tydPZ.js";import"./index-C9rmetsa.js";import"./index-CQJnhAbd.js";import"./index-BEn-qc9j.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const F=E("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const H=E("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]),Q={render:({children:G,...M})=>q.jsx(i,{...M,children:G}),component:i,title:"Forms/Button",tags:["autodocs"]},r={args:{variant:"default",children:"Button"}},e={args:{variant:"secondary",children:"Button"}},t={args:{variant:"destructive",children:"Button"}},a={args:{variant:"ghost",children:"Button"}},n={args:{variant:"link",children:"Button",as:"a",href:"#"}},s={args:{children:"Button",iconLeft:F}},o={args:{children:"Button",iconRight:H}},c={args:{children:"Button",disabled:!0}};var d,u,m;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: "default",
+    children: "Button"
+  }
+}`,...(m=(u=r.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var p,l,h;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    children: "Button"
+  }
+}`,...(h=(l=e.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};var g,B,f;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: "destructive",
+    children: "Button"
+  }
+}`,...(f=(B=t.parameters)==null?void 0:B.docs)==null?void 0:f.source}}};var v,L,S;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: "ghost",
+    children: "Button"
+  }
+}`,...(S=(L=a.parameters)==null?void 0:L.docs)==null?void 0:S.source}}};var y,k,x;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    variant: "link",
+    children: "Button",
+    as: "a",
+    href: "#"
+  }
+}`,...(x=(k=n.parameters)==null?void 0:k.docs)==null?void 0:x.source}}};var R,w,A;s.parameters={...s.parameters,docs:{...(R=s.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    iconLeft: ArrowLeft
+  }
+}`,...(A=(w=s.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var D,b,I;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    iconRight: ArrowRight
+  }
+}`,...(I=(b=o.parameters)==null?void 0:b.docs)==null?void 0:I.source}}};var j,_,z;c.parameters={...c.parameters,docs:{...(j=c.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    disabled: true
+  }
+}`,...(z=(_=c.parameters)==null?void 0:_.docs)==null?void 0:z.source}}};const T=["Default","Secondary","Destructive","Ghost","Link","IconLeft","IconRight","Disabled"];export{r as Default,t as Destructive,c as Disabled,a as Ghost,s as IconLeft,o as IconRight,n as Link,e as Secondary,T as __namedExportsOrder,Q as default};
---
+++ assets/header.stories-DTLRsue9.js
@@ -0,0 +1,75 @@
+import{j as e}from"./jsx-runtime-z8MfsBtr.js";import{c as x}from"./index-CQJnhAbd.js";import{M as n,U as a,B as i}from"./user-DQ10MMZF.js";import{A as l}from"./index-Dl2HYIcO.js";import{T as c}from"./index-CWVClC3q.js";import{B as M}from"./index-WPAokm3T.js";import{H as j}from"./index-BRgwz2oW.js";import{c as h}from"./createLucideIcon-BT4tydPZ.js";import{S as I}from"./settings-Dg-RlEhs.js";import{L as y}from"./life-buoy-BXarWrHS.js";import"./index-C9rmetsa.js";import"./index-C8beOrku.js";import"./index-BuZeDFlD.js";import"./index-BEn-qc9j.js";import"./index-CFGT8jr2.js";import"./index-BUIDNy1Y.js";import"./index-wgz3LQUQ.js";import"./index-BroPwuUX.js";import"./floating-ui.react-dom-DKjcifLt.js";import"./index-De76eAE3.js";import"./Combination-D8i3ZibE.js";const H=x({base:"w-24"});function f({className:o,...t}){return e.jsx("img",{className:H({className:o}),...t})}f.__docgenInfo={description:"",methods:[],displayName:"Image"};const N=x({base:"fixed left-0 top-0 w-full bg-zinc-950"});function p({children:o,className:t,...g}){return e.jsx("header",{className:N({className:t}),...g,children:e.jsx("div",{className:"m-auto flex w-full max-w-7xl items-center justify-between p-4",children:o})})}p.__docgenInfo={description:"",methods:[],displayName:"Root"};const s={Root:p,Image:f},w=""+new URL("logo-white-CAYzcsj3.svg",import.meta.url).href;/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const R=h("LogOut",[["path",{d:"M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4",key:"1uf3rs"}],["polyline",{points:"16 17 21 12 16 7",key:"1gabdz"}],["line",{x1:"21",x2:"9",y1:"12",y2:"12",key:"1uyos4"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=h("Menu",[["line",{x1:"4",x2:"20",y1:"12",y2:"12",key:"1e0a9i"}],["line",{x1:"4",x2:"20",y1:"6",y2:"6",key:"1owob3"}],["line",{x1:"4",x2:"20",y1:"18",y2:"18",key:"yk5zj1"}]]),G={component:s.Root,title:"Surfaces/Header",tags:["autodocs"]},r={render:({...o})=>e.jsxs(s.Root,{...o,children:[e.jsxs("div",{className:"flex items-center gap-4",children:[e.jsx(s.Image,{src:w,alt:"Super Festval"}),e.jsxs(n.Root,{children:[e.jsx(n.Trigger,{children:e.jsx(v,{size:24,className:"text-zinc-50"})}),e.jsxs(n.Portal,{children:[e.jsx(n.Header,{children:e.jsx(j,{children:"Indicações RH"})}),e.jsxs(n.Content,{children:[e.jsx(n.Item,{href:"#",icon:a,className:"rounded-t",children:"Home"}),e.jsx(n.Item,{href:"#",icon:i,className:"rounded-b",children:"Indicações"})]})]})]})]}),e.jsxs(n.Root,{children:[e.jsx(n.Trigger,{children:e.jsx(l,{image:"https://placehold.co/600x400",username:"User"})}),e.jsxs(n.Portal,{children:[e.jsxs(n.Header,{children:[e.jsx(l,{image:"https://placehold.co/600x400",username:"User"}),e.jsxs("div",{className:"flex flex-col",children:[e.jsx(c,{as:"strong",children:"Jhon Doe"}),e.jsx(c,{size:"xs",as:"small",children:"Administrador"})]})]}),e.jsxs(n.Content,{children:[e.jsx(n.Item,{href:"#",icon:a,children:"Perfil"}),e.jsx(n.Item,{href:"#",icon:i,children:"Notificações"}),e.jsx(n.Item,{href:"#",icon:I,children:"Configurações"})]}),e.jsxs(n.Footer,{children:[e.jsx(n.Item,{href:"#",icon:y,children:"Ajuda"}),e.jsx(M,{iconLeft:R,className:"w-full rounded-none rounded-b bg-yellow-600/25 text-yellow-800 hover:bg-yellow-600/50",children:"Sair"})]})]})]})]})};var m,d,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: ({
+    ...rest
+  }) => <Header.Root {...rest}>
+      <div className="flex items-center gap-4">
+        <Header.Image src={Logo} alt="Super Festval" />
+        <Menu.Root>
+          <Menu.Trigger>
+            <MenuIcon size={24} className="text-zinc-50" />
+          </Menu.Trigger>
+
+          <Menu.Portal>
+            <Menu.Header>
+              <Heading>Indicações RH</Heading>
+            </Menu.Header>
+            <Menu.Content>
+              <Menu.Item href="#" icon={User} className="rounded-t">
+                Home
+              </Menu.Item>
+              <Menu.Item href="#" icon={Bell} className="rounded-b">
+                Indicações
+              </Menu.Item>
+            </Menu.Content>
+          </Menu.Portal>
+        </Menu.Root>
+      </div>
+
+      <Menu.Root>
+        <Menu.Trigger>
+          <Avatar image="https://placehold.co/600x400" username="User" />
+        </Menu.Trigger>
+
+        <Menu.Portal>
+          <Menu.Header>
+            <Avatar image="https://placehold.co/600x400" username="User" />
+            <div className="flex flex-col">
+              <Text as="strong">Jhon Doe</Text>
+              <Text size="xs" as="small">
+                Administrador
+              </Text>
+            </div>
+          </Menu.Header>
+          <Menu.Content>
+            <Menu.Item href="#" icon={User}>
+              Perfil
+            </Menu.Item>
+            <Menu.Item href="#" icon={Bell}>
+              Notificações
+            </Menu.Item>
+            <Menu.Item href="#" icon={Settings}>
+              Configurações
+            </Menu.Item>
+          </Menu.Content>
+          <Menu.Footer>
+            <Menu.Item href="#" icon={LifeBuoy}>
+              Ajuda
+            </Menu.Item>
+            <Button iconLeft={LogOut} className="w-full rounded-none rounded-b bg-yellow-600/25 text-yellow-800 hover:bg-yellow-600/50">
+              Sair
+            </Button>
+          </Menu.Footer>
+        </Menu.Portal>
+      </Menu.Root>
+    </Header.Root>
+}`,...(u=(d=r.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const K=["Default"];export{r as Default,K as __namedExportsOrder,G as default};
