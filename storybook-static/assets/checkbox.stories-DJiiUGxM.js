e3f72708ea094b6ca00e60bcd318312ffe5f9286
daniellvaz <daniellmvaz@gmail.com>
2025-05-22 10:19:39 -0300

    feat: create pagination component

---
+++ storybook-static/assets/checkbox.stories-DJiiUGxM.js
@@ -0,0 +1,15 @@
+import{C as l}from"./index-CyVZGDX5.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";import"./index--qcDGAq6.js";import"./index-C66Dxnp2.js";import"./check-BExW4hvz.js";import"./createLucideIcon-DbwIO29z.js";const C={component:l,title:"Forms/Checkbox",argTypes:{disabled:{control:{type:"boolean"}},checked:{control:{type:"boolean"}}},tags:["autodocs"]},e={args:{checked:!0,disabled:!1}},r={args:{checked:!1}},s={args:{checked:!1,disabled:!0}};var a,o,c;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    disabled: false
+  }
+}`,...(c=(o=e.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var t,d,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    checked: false
+  }
+}`,...(n=(d=r.parameters)==null?void 0:d.docs)==null?void 0:n.source}}};var p,m,i;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    checked: false,
+    disabled: true
+  }
+}`,...(i=(m=s.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const y=["Checked","Unchecked","Disabled"];export{e as Checked,s as Disabled,r as Unchecked,y as __namedExportsOrder,C as default};
