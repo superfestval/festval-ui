a6871e9dfab117be56154d06d8c9f5ce7787855a
daniellvaz <daniellmvaz@gmail.com>
2025-11-28 20:00:53 +0000

    Deploying to gh-pages from @ superfestval/festval-ui@20b3b81f05dd47adaf0666d1fc44ed1bbc921ed3 🚀

---
+++ assets/heading.stories-CGpPtT7U.js
@@ -0,0 +1,7 @@
+import{H as o}from"./index-BRgwz2oW.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./index-CQJnhAbd.js";const p={component:o,title:"Typograph/Heading",args:{children:"Custom text"},argTypes:{children:{type:"string"},size:{control:{type:"inline-radio",labels:!1},options:["xs","sm","md","lg","xlg","2xl","4xl"]},as:{type:"string",control:{type:"select"},options:["h1","h2","h3","h4"]}},tags:["autodocs"]},e={args:{size:"4xl",children:"Title",as:"h1"}};var s,t,r;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    size: "4xl",
+    children: "Title",
+    as: "h1"
+  }
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,p as default};
