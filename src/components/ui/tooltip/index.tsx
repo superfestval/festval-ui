f58b6d9fedf52f7ffd043b61958e96409f25bfd6
daniellvaz <daniellmvaz@gmail.com>
2025-12-01 16:57:45 -0300

    fix

--- src/components/ui/tooltip/index.tsx
+++ src/components/ui/tooltip/index.tsx
@@ -4,13 +4,16 @@ import { ReactNode } from "react";
 export type TooltipProps = {
   children?: ReactNode;
   value: string;
+  asChild?: boolean;
 };
 
-export const Tooltip = ({ children, value }: TooltipProps) => {
+export const Tooltip = ({ value, children, asChild }: TooltipProps) => {
   return (
     <Primitive.Provider>
       <Primitive.Root>
-        <Primitive.Trigger className="w-full">{children}</Primitive.Trigger>
+        <Primitive.Trigger asChild={asChild} className="w-full">
+          {children}
+        </Primitive.Trigger>
         <Primitive.Portal>
           <Primitive.Content className="w-full select-none rounded bg-white px-[15px] py-2.5 text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]">
             {value}
