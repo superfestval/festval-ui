a24efbf68b7400424bc641f07771c3fa55d6b191
daniellvaz <daniellmvaz@gmail.com>
2026-01-22 12:26:36 +0000

    Deploying to gh-pages from @ superfestval/festval-ui@ff2dca6f1280d902b25993d68a7f1278858d989e 🚀

---
+++ assets/badge.stories-DdDeXpPs.js
@@ -0,0 +1,21 @@
+import{j as o}from"./jsx-runtime-z8MfsBtr.js";import{c as _}from"./index-CQJnhAbd.js";import"./index-C9rmetsa.js";const B=_({base:"flex w-fit items-center justify-center rounded-2xl px-4 py-1 text-xs",variants:{variant:{default:"border border-gray-300 bg-gray-200 text-gray-600",success:"border border-green-300 bg-green-200 text-green-800",warning:"border border-yellow-300 bg-yellow-200 text-yellow-800",danger:"border border-red-300 bg-red-200 text-red-800",info:"border border-blue-300 bg-blue-200 text-blue-800"}},defaultVariants:{variant:"default"}});function j({children:t,variant:D}){return o.jsx("div",{className:B({variant:D}),children:o.jsx("small",{children:t})})}j.__docgenInfo={description:"",methods:[],displayName:"Badge"};const N={render:({...t})=>o.jsx(j,{...t,children:"Badge"}),title:"Display/Badge",tags:["autodocs"]},r={args:{variant:"default"}},e={args:{variant:"success"}},a={args:{variant:"warning"}},s={args:{variant:"danger"}},n={args:{variant:"info"}};var c,d,i;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    variant: "default"
+  }
+}`,...(i=(d=r.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};var g,u,l;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: "success"
+  }
+}`,...(l=(u=e.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var m,p,b;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: "warning"
+  }
+}`,...(b=(p=a.parameters)==null?void 0:p.docs)==null?void 0:b.source}}};var f,x,v;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    variant: "danger"
+  }
+}`,...(v=(x=s.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var y,w,S;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    variant: "info"
+  }
+}`,...(S=(w=n.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};const V=["Default","Success","Warning","Danger","Info"];export{s as Danger,r as Default,n as Info,e as Success,a as Warning,V as __namedExportsOrder,N as default};
